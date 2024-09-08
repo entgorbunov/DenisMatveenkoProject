2d93f6a1890fd24b39bc427c3914143887b8924e
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-09-08 13:32:03 +0500

    maven project is done

---
+++ common/src/test/java/CommonTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CommonTest {
+
+    @Test
+    void testCommon() {
+        Assertions.assertTrue(true);
+    }
+}
